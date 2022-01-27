c85e0a6358481718a58d4dbc55421ef69cd86d08
Ella <ellapark@live.unc.edu>
2022-01-27 11:30:03 -0500

    assignment 0, hello world complete

--- src/a0/Main.java
+++ src/a0/Main.java
@@ -5,13 +5,14 @@ public class Main {
         // Prints out your Java version
         // Make sure it is 10.0 or higher
         System.out.println("Your Java version: " + System.getProperty(("java.version")));
-
         // Task 2 here
-
+        System.out.println(sayHello());
     }
 
     // Task 1 here
-
+    public static String sayHello() {
+        return "Hello World";
+    }
 
 
 }
